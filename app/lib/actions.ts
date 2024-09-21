d8fe4a89935675e9d4716a6b9cea53658a08adce
yash49 <49yash@gmail.com>
2024-09-22 04:39:51 +0530

    cleaup

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,7 +34,7 @@ export async function createInvoice(formData: FormData) {
           INSERT INTO invoices (customer_id, amount, status, date)
           VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
-      } catch (error) {
+      } catch  {
         return {
           message: 'Database Error: Failed to Create Invoice.',
         };
@@ -62,7 +62,7 @@ export async function updateInvoice(id: string, formData: FormData) {
             SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
             WHERE id = ${id}
           `;
-      } catch (error) {
+      } catch  {
         return { message: 'Database Error: Failed to Update Invoice.' };
       }
     revalidatePath('/dashboard/invoices');
@@ -71,13 +71,12 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   export async function deleteInvoice(id: string) {
 
-    throw new Error('Failed to Delete Invoice');
 
     try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
       return { message: 'Deleted Invoice.' };
-    } catch (error) {
+    } catch  {
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
   }
