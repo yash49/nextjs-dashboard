909f961e897b9963592d261b8efc2f43a89aa07b
yash49 <49yash@gmail.com>
2024-09-22 19:03:34 +0530

    add auth\

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,9 @@
 'use server';
 
 
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -110,3 +112,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
   }
+
+
+  
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
