eaea8cf33bdab866862efdc2361cb400da868194
yash49 <49yash@gmail.com>
2024-09-22 20:17:45 +0530

    add meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+  };
+
+
 export default function Page() {
     return <p>Customers Page</p>;
   }
